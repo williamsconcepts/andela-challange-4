49589b2058a3fdd60d22b0e29a3967f33d9e81e6
williams <chidiekwebelam@.gmail.com>
2020-04-16 07:12:21 -0700

    Updates

---
+++ precache-manifest.5ba42d701d83bd192bd9eebb77095dfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b1475cdca6841300a13a9697d4c2ad",
+    "url": "/andela-challenge4/index.html"
+  },
+  {
+    "revision": "913c95ede06a825ae3f4",
+    "url": "/andela-challenge4/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b760d2b74642f35c392e",
+    "url": "/andela-challenge4/static/css/main.37af7d79.chunk.css"
+  },
+  {
+    "revision": "913c95ede06a825ae3f4",
+    "url": "/andela-challenge4/static/js/2.cd024349.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andela-challenge4/static/js/2.cd024349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b760d2b74642f35c392e",
+    "url": "/andela-challenge4/static/js/main.be809225.chunk.js"
+  },
+  {
+    "revision": "9e26cb71d66826b49e63",
+    "url": "/andela-challenge4/static/js/runtime-main.cf688c1d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/andela-challenge4/static/media/SEGA.12a3db8b.woff"
+  }
+]);
